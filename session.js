68cc5a703157ebe54a714f4cc00aa7c9d3bbb906
dilabot545 <samannawarathnanawarathna@gmail.com>
2025-03-08 22:31:10 +0530

    Update session.js

--- session.js
+++ session.js
@@ -43,10 +43,5 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~KYsH0Zaa#1GpArHn_0xahWOTh6hffrBUh6nC7s5vdDrE3UBnUuZo",
 SUDO: process.env.SUDO || ["94717775628","94758775628"],
 PORT: process.env.PORT || "8000",
-FOOTER: process.env.FOOTER || "✦◆◉━━━━━━━━━━━━━━━━━━━━━━━━━━━━━◉◆✦
-❖❯❯ 𝗝𝗢𝗜𝗡 𝗠𝗩 𝗚𝗥𝗢𝗨𝗣 ➤ https://chat.whatsapp.com/Lp91cySvXueFChmygbmdCF
-❖❯❯ 𝗝𝗢𝗜𝗡 𝗕𝗢𝗧 𝗚𝗥𝗢𝗨𝗣 ➤ https://chat.whatsapp.com/ETnlbJGBTd13TvGhSUIFSq
-❖❯❯ 𝗝𝗢𝗜𝗡 𝗖𝗔𝗥𝗧𝗢𝗢𝗡 𝗚𝗥𝗢𝗨𝗣 ➤
-https://chat.whatsapp.com/CurZjvmwN735el7Wdomqsl
-✦◆◉━━━━━━━━━━━━━━━━━━━━━━━━━━━━━◉◆✦",
+FOOTER: process.env.FOOTER || "*⦿ ᗰIᑌՏIC ᑕᕼᗩᑎᗴᒪ ᒪIᑎK⇨*https://whatsapp.com/channel/0029Vb3uqklAInPchBUjck3l *⦿ ᗪK ᗰᐯ ᘜᖇOᑌᑭᗴ ᒪIᑎK⇨* https://chat.whatsapp.com/Lp91cySvXueFChmygbmdCF *⦿ ᗪK ᑕᗩᖇTOOᑎ ᘜᖇOᑌᑭᗴ ᒪIᑎK⇨* https://chat.whatsapp.com/CurZjvmwN735el7Wdomqsl *⦿ ᗷOT ᘜᖇOᑌᑭᗴ ᒪIᑎK ⇨* https://chat.whatsapp.com/ETnlbJGBTd13TvGhSUIFSq",
 };
