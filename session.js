576d9dd63f7c34268db7698bd8e996041196f663
dilabot545 <samannawarathnanawarathna@gmail.com>
2025-03-10 18:47:41 +0530

    Update session.js

--- session.js
+++ session.js
@@ -40,7 +40,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~DFljFQJK#RrGXUyfGq9ZnzoFXy4ItXOArj23R9l6nNqIeFFYrLMQ",
+SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~zd8mAQbQ#sN_uAY7yk1noSIjG33Q02BETxDgaZPk1XBv2ZukhgbY",
 SUDO: process.env.SUDO || ["94717775628","94758775628"],
 PORT: process.env.PORT || "8000", 
 FOOTER: process.env.FOOTER || "> 📥 ᵈᵒʷⁿˡᵒᵃᵈ ʷⁱᵗʰⁱⁿᵍ 14 ᵈᵃʸˢ.❗                                                               *`✨️𝘿𝙆 𝙈𝙊𝙑𝙄𝙀𝙎✨️`*                                                                                                                                                    *❖◉◎__[D̲̅][I̲̅][L̲̅][A̲̅]__◎◉❖*",
